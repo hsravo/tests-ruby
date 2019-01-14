cd205786f717317d2fe82ac9428d9de2e6008081
Steph Ravo <hsravo1@gmail.com>
2019-01-14 11:49:59 +0100

    ajout temperature

---
+++ lib/01_temperature.rb
@@ -0,0 +1,9 @@
+def ftoc(x)
+	return celsius = (x - 32) * 5 / 9
+ 	puts "Temperature in celsius: #{celsius}"
+end
+
+def ctof(y)
+	return fahrenheit = (y * 1.8) + 32
+	puts "Temperature in fahrenheit : #{fahrenheit}"
+end
